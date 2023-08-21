2640b14c1721c30a79a3859a6209a62898552c50
Nupur Agrawal <nupur202000@gmail.com>
2023-08-21 16:50:47 +0530

    add function to generate build status message

--- pkg/status/status.go
+++ pkg/status/status.go
@@ -1,16 +1,23 @@
 package status
 
+import "strings"
+
 type Stage string
 type State string
 
 const (
-	CONFIGURE Stage = "configure"
+	// Stages of build process
+	CONFIGURE   Stage = "configure"
 	APK_INSTALL Stage = "apk-install"
-	BUILD Stage = "build"
+	BUILD       Stage = "build"
+
+	// States of build's stages
+	FAILED     State = "failed"
+	INPROGRESS State = "InProgress"
+	SUCCESS    State = "success"
 )
 
-const (
-	FAILED State = "failed"
-	INPROGRESS State = "InProgress"
-	SUCCESS State = "success"
-)
+func GenerateBuildMsg(stage Stage, state State, msg string) string {
+	statusMsg := []string{string(stage), string(state), msg}
+	return strings.Join(statusMsg, "|")
+}
